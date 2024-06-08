4ea8c0a677468ccf957d76ce52186ab4dc81593c
Guilherme Orlandin <110955432+GuiOrlandin@users.noreply.github.com>
2024-06-08 16:29:00 -0300

    readme adicionado

---
+++ README.md
@@ -0,0 +1,21 @@
+# APP
+
+Parte 1 do desáfio para Desenvolvedor Full-Stack Web.
+
+### Instale as dependências
+
+```sh
+npm i
+```
+
+### Inicie a aplicação
+
+```sh
+npm run start:dev
+```
+
+### Rode os testes da aplicação
+
+```sh
+npm run test
+```
--- src/auth/dto/authDto.ts
+++ src/auth/dto/authDto.ts
@@ -1,4 +1,13 @@
-export class AuthDto {
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { Auth } from '../entities/auth';
+
+export class AuthDto extends Auth {
+  @IsNotEmpty()
+  @IsEmail()
+  @IsString()
   email: string;
+
+  @IsNotEmpty()
+  @IsString()
   password: string;
 }
